8e9cdbedfc8496f5e3b8ff854a9fcbd3a5910e89
Aiman Fatima <aimanfatimadl@gmail.com>
2021-08-27 01:43:19 +0530

    useEffect hook

--- src/pages/AllMeetUps.js
+++ src/pages/AllMeetUps.js
@@ -1,19 +1,32 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import MeetUpList from "../components/meetups/MeetUpList";
 
 function AllMeetUpsPage() {
   const [isLoading, setIsLoading] = useState(true);
-  const [loadedMeetups, setLoadedMeetups] = useState([]]);
-  
-  fetch(
-    "https://react-getting-started-13467-default-rtdb.firebaseio.com/meetups.json"
-  ).then(response => {
-    return response.json();
-  }).then(data => {
-    setIsLoading(false);
-    setLoadedMeetups(data)
-  });
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
 
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-getting-started-13467-default-rtdb.firebaseio.com/meetups.json"
+    ).then(response => {
+      return response.json();
+    }).then(data => {
+      const meetups = [];
+
+      for (const key in data) {
+        const meetup = {
+          id: key,
+          ...data[key]
+        };
+        meetups.push(meetup)
+      }
+
+      setIsLoading(false);
+      setLoadedMeetups(meetups);
+    });
+  }, [])
+  
   if (isLoading) {
     return <section>
       <p>Loading...</p>
