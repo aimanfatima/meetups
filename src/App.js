c79aa036d087dbac6266ff2dfb1cb31f292a014b
Aiman Fatima <aimanfatimadl@gmail.com>
2021-08-27 00:53:53 +0530

    getting user data and handling form submission

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Route, Switch } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-import MainNavigation from "./components/layout/MainNavigation";
 import AllMeetUpsPage from "./pages/AllMeetUps";
 import Favourites from "./pages/Favourites";
 import NewMeetup from "./pages/NewMeetup";
