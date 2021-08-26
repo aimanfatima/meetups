530d0bfa18c1fa60857d74aa1964d2c014b794c3
Aiman Fatima <aimanfatimadl@gmail.com>
2021-08-26 23:06:38 +0530

    New starting project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import Todo from './components/Todo'
 
 function App() {
   return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text='Learn React' />
-      <Todo text='Master React'/>
-      <Todo text='Explore full React'/>
-    </div>
+    <div></div>
   );
 }
 
